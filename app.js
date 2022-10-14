444111bcdbb1cac061fffd6ae6a1d9bdd32f714c
Iago Chiapetta <iagochiapetta@gmail.com>
2022-10-14 09:46:24 -0300

    Primeiro Commit
    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function instagram() {
+    window.location.href = "https://instagram.com/couttmodas?igshid=YmMyMTA2M2Y=";
+}
+
+function whatsapp() {
+    window.location.href = "http://Wa.me/5521973131161";
+}
